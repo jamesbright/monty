ec340e0880c4e744050a09cff131f27e94b9c3d6
James Ononiwu <jamesononiwu@gmail.com>
2023-01-18 07:51:12 -0800

    updated divop

--- instruct_math.c
+++ instruct_math.c
@@ -56,25 +56,21 @@ free(temp);
  */
 void divop(stack_t **stack, unsigned int nline)
 {
-stack_t *temp;
+int tmp;
 
-if (!(*stack) || !(*stack)->next)
+if (*stack == NULL || (*stack)->next == NULL)
 {
-fprintf(stderr, "L%d: can't div, stack too short\n", nline);
+printf("L%u: can't div, stack too short\n", nline);
 exit(EXIT_FAILURE);
 }
-
-if ((*stack)->n == 0)
+tmp = (*stack)->n;
+if (tmp == 0)
 {
-fprintf(stderr, "L%d: division by zero\n", nline);
+printf("L%u: division by zero\n", nline);
 exit(EXIT_FAILURE);
 }
-
-temp = *stack;
-(*stack)->next->n /= (*stack)->n;
-*stack = (*stack)->next;
-(*stack)->prev = NULL;
-free(temp);
+pop(stack, nline);
+(*stack)->n /= tmp;
 }
 
 /**
